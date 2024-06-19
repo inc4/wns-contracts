56ab70b9f6729363fbd08bc02c4c0fea16605ec4
Danil1996 <danilkit48@gmail.com>
2024-06-19 16:39:47 +0300

    style(deploy): remove unused modules

--- deploy/ethregistrar/00_deploy_base_registrar_implementation.ts
+++ deploy/ethregistrar/00_deploy_base_registrar_implementation.ts
@@ -2,7 +2,6 @@ import namehash from 'eth-ens-namehash'
 import { ethers } from 'hardhat'
 import { DeployFunction } from 'hardhat-deploy/types'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
-import { keccak256 } from 'js-sha3'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { getNamedAccounts, deployments, network } = hre
